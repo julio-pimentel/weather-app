9b9b11d8b792512817183a3a9c34c6a24a964fa5
Julio Pimentel <pimentel.julio@outlook.com>
2022-09-07 11:39:35 +1000

    Modify ALB and ECS values 2

--- modules/alb/alb.tf
+++ modules/alb/alb.tf
@@ -1,10 +1,10 @@
 resource "aws_alb" "alb" {
   name = var.alb_name
-  internal = false
   load_balancer_type = "application"
   security_groups = [var.alb_sg_id]
   subnets = [var.pub_cidr_id_1, var.pub_cidr_id_2]
 
+  internal = false
   enable_deletion_protection = true
 
   tags = {
@@ -14,7 +14,7 @@ resource "aws_alb" "alb" {
 
 resource "aws_alb_target_group" "alb_tg" {
   name = var.alb_tg_name
-  port = 80
+  port = var.container_port
   protocol = "HTTP"
   vpc_id = var.vpc_id
   target_type = "ip"
--- modules/ecs/ecs_service.tf
+++ modules/ecs/ecs_service.tf
@@ -9,27 +9,25 @@ resource "aws_ecs_service" "ecs_service" {
   name = var.ecs_service_name
   cluster = aws_ecs_cluster.ecs_cluster.id
   task_definition = aws_ecs_task_definition.ecs_task.arn
-  desired_count = 3
-  deployment_minimum_healthy_percent = 50
-  deployment_maximum_percent = 150
-  health_check_grace_period_seconds = 60
+  desired_count = 2
   launch_type = "FARGATE"
   scheduling_strategy = "REPLICA"
 
   network_configuration {
-    security_groups  = var.ecs_tasks_sg
-    subnets = [var.pub_cidr_id_1, var.pub_cidr_id_2]
+    security_groups  = [var.ecs_tasks_sg]
+    subnets = [var.priv_cidr_id_1, var.priv_cidr_id_2]
     assign_public_ip = false
   }
 
   load_balancer {
     target_group_arn = var.alb_tg_arn
     container_name   = "${var.container_name}"
-    container_port   = 80
-    hostPort = var.container_port
+    container_port   = var.container_port
   }
 
+/*
   lifecycle {
     ignore_changes = [task_definition, desired_count]
   }
+*/
 }

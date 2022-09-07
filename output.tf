847ce0c0dd2644d6668ff4efb27ba7d6aa71153c
Julio Pimentel <pimentel.julio@outlook.com>
2022-09-07 17:37:41 +1000

    Remove s3 outputs tf

--- output.tf
+++ output.tf
@@ -1,12 +1,3 @@
-output "bucket_name" {
-  description = "The name of the bucket"
-  value       = ["${module.s3_bucket.s3_bucket_name}"]
-}
-
-output "bucket_name_arn" {
-  description = "The name of the bucket"
-  value       = ["${module.s3_bucket.s3_bucket_name_arn}"]
-}
 
 /*
 output "alb_dns_name" {
